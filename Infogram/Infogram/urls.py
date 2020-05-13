d35e2046af8087312f87851bf20aa8f207870912
Ana Lagunas <aplagunitas@comunidad.unam.mx>
2020-05-13 17:07:31 -0500

    views

--- Infogram/Infogram/urls.py
+++ Infogram/Infogram/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from dashboard.views import IndexView, LoginView, RegisterView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
